c5ce678252cd735a3be45da4fd788c042e7991d8
sparesparrow <dallheimal@gmail.com>
2025-07-03 06:25:28 +0200

    chore: workspace/build config cleanup, remove obsolete files, document workspace best practices

--- dist-tsc/tests/setup.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function closeServer(server: any): Promise<void>;
-export declare function closeEventSource(es: any): Promise<void>;
--- dist-tsc/tests/unit/utils.unit.test.js
+++
@@ -1,8 +0,0 @@
-describe.skip('Utils', () => {
-    describe('jsonFriendlyErrorReplacer', () => {
-        it('should serialize a standard Error object', () => {
-            // ... existing code ...
-        });
-    });
-});
-export {};
